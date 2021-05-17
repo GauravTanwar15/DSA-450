82cdb3bb88a7f29e23141e8b5040377393250590
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-05-17 15:59:50 +0530

    Add array

---
+++ Array/Max-Min.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+struct Array
+{
+    int A[20];
+    int size;
+    int length;
+};
+void Display(struct Array arr)
+{
+    int i;
+    cout<<endl<<"elements of array "<<endl;
+    for ( i = 0; i < arr.length; i++)
+    {
+        printf("%d ",arr.A[i]);
+    }
+    cout<<endl;   
+};
+
+int Max(struct Array arr)
+{
+    int max= arr.A[0];
+    for (int i = 0; i < arr.length; i++)
+    {
+        if(arr.A[i]>max)
+        max=arr.A[i];
+    }
+    return max;    
+}
+
+int Min(struct Array arr)
+{
+    int min= arr.A[0];
+    for (int i = 0; i < arr.length; i++)
+    {
+        if(arr.A[i]<min)
+        min=arr.A[i];
+    }
+    return min;    
+}
+
+int main(){
+
+struct Array arr={{10,343,200,500,2},20,5};
+Display(arr);
+cout<<"Maximum element is: "<<Max(arr)<<endl;
+cout<<"Minimum element is: "<<Min(arr);
+
+
+return 0;
+};
