c53dac5db428b12bd1a6b3c5d1156e900fae1ebc
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-06-29 20:03:59 +0530

    add array

--- Array/kadane-algo.cpp
+++ Array/kadane-algo.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       int sum = 0;
+        int maxi = INT_MIN;
+        for(auto it: nums)
+        {
+            sum+= it;
+            maxi = max(sum, maxi);
+            if(sum<0) sum=0;
+        }
+        return maxi;
+    }
+};
 int main(){
 
 return 0;
