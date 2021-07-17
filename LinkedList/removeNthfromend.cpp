84a81e69d184793e6c2b5a83144bda15dcdff069
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-07-17 15:38:58 +0530

    add LinkedList

--- LinkedList/removeNthfromend.cpp
+++ LinkedList/removeNthfromend.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 using namespace std;
+  struct ListNode {
+      int val;
+     ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode * start = new ListNode();
+        start -> next = head;
+        ListNode* fast = start;
+        ListNode* slow = start;     
+
+        for(int i = 1; i <= n; ++i)
+            fast = fast->next;
+    
+        while(fast->next != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        
+        slow->next = slow->next->next;
+        
+        return start->next;
+    }
+}
 int main(){
 
 return 0;
