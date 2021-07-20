1cd52c7b2207c7a1d208da83a9cd2a1a8354148d
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-07-20 14:43:39 +0530

    add LinkedList

--- LinkedList/watercollection.cpp
+++ LinkedList/watercollection.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 using namespace std;
+class Solution {
+public:
+    int trap(vector<int>& height) {
+         
+        int n = height.size(); 
+        int left=0; int right=n-1;
+        int res=0;
+        int maxleft=0, maxright=0;
+        
+        while(left<=right){
+            
+            if(height[left]<=height[right]){
+
+                if(height[left]>=maxleft) maxleft=height[left];
+                else res+=maxleft-height[left];
+                
+                left++;
+            }
+            else{
+
+                if(height[right]>=maxright) maxright= height[right];
+                else res+=maxright-height[right];
+                
+                right--;
+            }
+        }
+        return res;
+    }
+};
 int main(){
 
 return 0;
