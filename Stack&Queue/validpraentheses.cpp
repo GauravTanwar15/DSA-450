dbc802ed836c257e91320babba65b710062ea960
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-07-25 20:21:51 +0530

    add stack & queue

---
+++ Stack&Queue/validpraentheses.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st; 
+        for(auto it: s) {
+            if(it=='(' || it=='{' || it == '[') st.push(it); 
+            else {
+                if(st.size() == 0) return false; 
+                char ch = st.top(); 
+                st.pop(); 
+                if((it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{')) continue;
+                else return false;
+            }
+        }
+        return st.empty(); 
+    }
+};
+int main(){
+
+return 0;
+}
