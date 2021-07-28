383f5b1e748d3581ae831e20a6f3446ff6b72902
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-07-28 16:29:39 +0530

    add Binary Tree

---
+++ Binary Trees/ReverseLevelorder.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> ret;
+        if (root == NULL) return ret;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty()) {
+            int l = q.size();
+            vector<int> curlayer;
+            for (int i = 0; i < l; i++) {
+                TreeNode *cur = q.front();
+                q.pop();
+                curlayer.push_back(cur->val);
+                if (cur->left != NULL) q.push(cur->left);
+                if (cur->right != NULL) q.push(cur->right);
+            }
+            ret.push_back(curlayer);
+        }
+        reverse(ret.begin(), ret.end());
+        return ret;
+    }
+};
+int main(){
+
+return 0;
+}
