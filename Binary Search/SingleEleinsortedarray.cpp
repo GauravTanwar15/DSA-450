78e0222a11a9c5cb1bd838d42e56f4a50f700202
Gaurav Tanwar <gaurav.tanwar19@bvicam.in>
2021-08-12 00:06:46 +0530

    Add Binary Search

--- Binary Search/SingleEleinsortedarray.cpp
+++ Binary Search/SingleEleinsortedarray.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 using namespace std;
+class Solution {
+public:
+    
+int singleNonDuplicate(vector<int>& nums) {
+        int low = 0, high = nums.size() - 2; 
+        while(low <= high) {
+            int mid = (low + high) >> 1; 
+            if(nums[mid] == nums[mid^1]) {
+                low = mid + 1; 
+            } 
+            else {
+                high = mid - 1; 
+            }
+        }
+        return nums[low]; 
+    }
+};
 int main(){
 
 return 0;
